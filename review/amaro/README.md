22b5e947a89f6a1eb08a1dcad51e1a8b6394af3c
Luiz <lucachaves@gmail.com>
2023-03-06 10:50:14 -0300

    amaron, syntax json

---
+++ review/amaro/README.md
@@ -0,0 +1,1 @@
+https://ifpb.github.io/challenges/web/front-end/js/amaro/
---
+++ review/syntax/json.js
@@ -0,0 +1,8 @@
+const name = 'John';
+const email = 'john@email.com';
+
+let person = ['John', 'john@email.com'];
+console.log(person[1]);
+
+person = { name: 'John', email: 'john@email.com' };
+console.log(person.email);
