549cbc7c652678553a9805851cbe308703074620
Luiz <lucachaves@gmail.com>
2023-03-27 09:29:44 -0300

    parâmetros da requisição em express

--- classroom/express/hello-api/src/index.js
+++ classroom/express/hello-api/src/index.js
@@ -5,6 +5,8 @@ const server = express();
 
 server.use(morgan('tiny'));
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send('Hello World');
 });
@@ -12,6 +14,46 @@ server.get('/', (req, res) => {
 server.get('/ola', (req, res) => {
   res.send('Olá, mundo!');
 });
+
+// Query parameters
+// GET /hello/en?name=John
+server.get('/hello/en', (req, res) => {
+  const name = req.query.name;
+
+  const result = {
+    message: `Hello, ${name}!`,
+  };
+
+  res.json(result);
+});
+
+// Route parameters
+// GET /hello/pt/John
+server.get('/hello/pt/:name', (req, res) => {
+  const name = req.params.name;
+
+  const result = {
+    message: `Olá, ${name}!`,
+  };
+
+  res.json(result);
+});
+
+// Body parameters
+// POST /hello/es
+server.post('/hello/es', (req, res) => {
+  const name = req.body.name;
+
+  const result = {
+    message: `¡Hola, ${name}!`,
+  };
+
+  res.json(result);
+});
+
+// 404 handler
+
+// Error handler
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
