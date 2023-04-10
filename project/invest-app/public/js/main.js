138b2c8223eef6ba80bde468f1143921c3e37e27
Luiz <lucachaves@gmail.com>
2023-04-10 09:45:06 -0300

    ajuste no cadastro e remoção de invesitmentos

--- project/invest-app/public/js/main.js
+++ project/invest-app/public/js/main.js
@@ -1,11 +1,18 @@
+function formatCurrency(value) {
+  return value.toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+}
+
 function getInvestimentView(investiment) {
-  return `<div class="col">
-    <div class="card" id="investiment-${investiment.id}">
+  return `<div class="col" id="investiment-${investiment.id}">
+    <div class="card">
       <div class="card-header">
         ${investiment.name}
         <div class="icon-trash" style="float: right; display: inline">
-          <span 
-            class="iconify" 
+          <span
+            class="iconify"
             data-icon="solar:trash-bin-minimalistic-broken"
             style="font-size: 1.2rem"
           >
@@ -13,66 +20,72 @@ function getInvestimentView(investiment) {
         </div>
       </div>
       <div class="card-body">
-        <span>${investiment.value}</span>
+        <span class="fw-bold">Valor:</span> ${formatCurrency(investiment.value)}
       </div>
     </div>
   </div>`;
 }
 
-function addNewInvestimentView(investiment) {
-  const view = getInvestimentView(investiment);
+function addInvestimentView(investiment) {
+  const investimentsGrid = document.querySelector('#investiment-grid');
 
-  document.querySelector('#investiment-grid').innerHTML += view;
+  const investimentView = getInvestimentView(investiment);
+
+  investimentsGrid.insertAdjacentHTML('beforeend', investimentView);
+
+  const investimentCard = document.querySelector(
+    `#investiment-${investiment.id}`
+  );
+
+  investimentCard.querySelector('.icon-trash').onclick = () => {
+    fetch(`/investiments/${investiment.id}`, {
+      method: 'delete',
+    });
+
+    investimentCard.remove();
+  };
 }
 
-function loadRemoveEvent() {
-  const investimentCards = document.querySelectorAll('.card');
+async function loadInvestiments() {
+  const response = await fetch('/investiments');
 
-  for (const card of investimentCards) {
-    const id = card.id.replace('investiment-', '');
+  const investiments = await response.json();
 
-    card.querySelector('.icon-trash').onclick = () => {
-      fetch(`/investiments/${id}`, {
-        method: 'delete',
-      });
-
-      card.remove();
-    };
+  for (const investiment of investiments) {
+    addInvestimentView(investiment);
   }
 }
 
-const response = await fetch('/investiments');
-const investiments = await response.json();
+function loadFormSubmit() {
+  const form = document.querySelector('form');
 
-let investimentView = '';
+  form.onsubmit = async (event) => {
+    event.preventDefault();
 
-for (const investiment of investiments) {
-  investimentView += getInvestimentView(investiment);
+    const name = document.querySelector('#name').value.replace(',', '.');
+
+    const value = Number(document.querySelector('#value').value);
+
+    const investiment = { name, value };
+
+    const response = await fetch('/investiments', {
+      method: 'post',
+      body: JSON.stringify(investiment),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const newInvestiment = await response.json();
+
+    addInvestimentView(newInvestiment);
+
+    form.reset();
+
+    document.querySelector('#offcanvas-close').click();
+  };
 }
 
-loadRemoveEvent();
+loadInvestiments();
 
-document.querySelector('#investiment-grid').innerHTML = investimentView;
-
-document.querySelector('#submit').onclick = (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#name').value;
-  const value = Number(document.querySelector('#value').value);
-
-  const investiment = { name, value };
-
-  fetch('/investiments', {
-    method: 'post',
-    body: JSON.stringify(investiment),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  addNewInvestimentView(investiment);
-
-  loadRemoveEvent();
-
-  document.querySelector('#offcanvas-close').click();
-};
+loadFormSubmit();
