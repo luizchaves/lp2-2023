6900764e26a3ef42691aad117ec6f01ffccae6c7
Luiz <lucachaves@gmail.com>
2023-09-18 08:27:01 -0300

    validation

--- project/invest-app/public/js/signup.js
+++ project/invest-app/public/js/signup.js
@@ -5,21 +5,45 @@ const form = document.querySelector('form');
 async function handleSubmit(event) {
   event.preventDefault();
 
-  const user = Object.fromEntries(new FormData(form));
+  if (form.checkValidity()) {
+    const user = Object.fromEntries(new FormData(form));
 
-  const config = {
-    method: 'post',
-    body: JSON.stringify(user),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+    const config = {
+      method: 'post',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
-  const response = await fetch('/users', config);
+    const response = await fetch('/users', config);
 
-  if (response.ok) {
-    location.href = '/signin.html';
+    if (response.ok) {
+      location.href = '/signin.html';
+    } else {
+      console.log('Error no cadastro');
+    }
   } else {
-    console.log('Error no cadastro');
+    form.classList.add('was-validated');
   }
 }
+
+form.confirmationPassword.oninput = () => {
+  const password = form.password.value;
+
+  const confirmationPassword = form.confirmationPassword.value;
+
+  if (password !== confirmationPassword) {
+    const error = 'As senhas não são iguais.';
+
+    const confirmationPasswordError = document.querySelector(
+      '#confirmationPassword + .invalid-feedback'
+    );
+
+    confirmationPasswordError.textContent = error;
+
+    form.confirmationPassword.setCustomValidity(error);
+  } else {
+    form.confirmationPassword.setCustomValidity('');
+  }
+};
--- project/invest-app/src/routes.js
+++ project/invest-app/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { z } from 'zod';
 import Investment from './models/Investment.js';
 import Category from './models/Category.js';
 import User from './models/User.js';
 
+import { validate } from './middleware/validate.js';
 import { isAuthenticated } from './middleware/auth.js';
 
 const saltRounds = Number(process.env.SALT_ROUNDS);
@@ -22,22 +24,35 @@ router.get('/', (req, res) => {
   res.redirect('/home.html');
 });
 
-router.post('/investments', isAuthenticated, async (req, res) => {
-  const investment = req.body;
+router.post(
+  '/investments',
+  isAuthenticated,
+  validate(
+    z.object({
+      body: z.object({
+        name: z.string(),
+        value: z.number(),
+        categoryId: z.number(),
+      }),
+    })
+  ),
+  async (req, res) => {
+    const investment = req.body;
 
-  const userId = req.userId;
+    const userId = req.userId;
 
-  const newInvestment = await Investment.create({
-    ...investment,
-    userId,
-  });
+    const newInvestment = await Investment.create({
+      ...investment,
+      userId,
+    });
 
-  if (newInvestment) {
-    res.json(newInvestment);
-  } else {
-    throw new HTTPError('Invalid data to create investment', 400);
+    if (newInvestment) {
+      res.json(newInvestment);
+    } else {
+      throw new HTTPError('Invalid data to create investment', 400);
+    }
   }
-});
+);
 
 router.get('/investments', isAuthenticated, async (req, res) => {
   const userId = req.userId;
@@ -47,35 +62,57 @@ router.get('/investments', isAuthenticated, async (req, res) => {
   res.json(investments);
 });
 
-router.put('/investments/:id', isAuthenticated, async (req, res) => {
-  const id = Number(req.params.id);
+router.put(
+  '/investments/:id',
+  isAuthenticated,
+  validate(
+    z.object({
+      params: z.object({
+        id: z.number(),
+      }),
+    })
+  ),
+  async (req, res) => {
+    const id = Number(req.params.id);
 
-  const investment = req.body;
+    const investment = req.body;
 
-  const userId = req.userId;
+    const userId = req.userId;
 
-  if (id && investment) {
-    const newInvestment = await Investment.update({
-      ...investment,
-      userId,
-      id,
-    });
+    if (id && investment) {
+      const newInvestment = await Investment.update({
+        ...investment,
+        userId,
+        id,
+      });
 
-    res.json(newInvestment);
-  } else {
-    throw new HTTPError('Invalid data to update investment', 400);
+      res.json(newInvestment);
+    } else {
+      throw new HTTPError('Invalid data to update investment', 400);
+    }
   }
-});
+);
 
-router.delete('/investments/:id', isAuthenticated, async (req, res) => {
-  const id = Number(req.params.id);
+router.delete(
+  '/investments/:id',
+  isAuthenticated,
+  validate(
+    z.object({
+      params: z.object({
+        id: z.number(),
+      }),
+    })
+  ),
+  async (req, res) => {
+    const id = Number(req.params.id);
 
-  if (id && (await Investment.remove(id))) {
-    res.sendStatus(204);
-  } else {
-    throw new HTTPError('Id is required to remove investment', 400);
+    if (id && (await Investment.remove(id))) {
+      res.sendStatus(204);
+    } else {
+      throw new HTTPError('Id is required to remove investment', 400);
+    }
   }
-});
+);
 
 router.get('/categories', isAuthenticated, async (req, res) => {
   const categories = await Category.readAll();
@@ -83,45 +120,69 @@ router.get('/categories', isAuthenticated, async (req, res) => {
   res.json(categories);
 });
 
-router.post('/users', async (req, res) => {
-  const user = req.body;
+router.post(
+  '/users',
+  validate(
+    z.object({
+      params: z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(8),
+        confirmationPassword: z.string().min(8),
+      }),
+    })
+  ),
+  async (req, res) => {
+    const user = req.body;
 
-  delete user.confirmationPassword;
+    delete user.confirmationPassword;
 
-  const hash = await bcrypt.hash(user.password, saltRounds);
+    const hash = await bcrypt.hash(user.password, saltRounds);
 
-  user.password = hash;
+    user.password = hash;
 
-  const newUser = await User.create(user);
+    const newUser = await User.create(user);
 
-  res.status(201).json(newUser);
-});
-
-router.post('/signin', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.readByEmail(email);
-
-    const { id: userId, password: hash } = user;
-
-    const match = await bcrypt.compare(password, hash);
-
-    if (match) {
-      const token = jwt.sign(
-        { userId },
-        process.env.JWT_SECRET,
-        { expiresIn: 3600 } // 1h
-      );
-
-      res.json({ auth: true, token });
-    } else {
-      throw new Error('User not found');
-    }
-  } catch (error) {
-    res.status(401).json({ error: 'User not found' });
+    res.status(201).json(newUser);
   }
-});
+);
+
+router.post(
+  '/signin',
+  validate(
+    z.object({
+      params: z.object({
+        email: z.string().email(),
+        password: z.string().min(8),
+      }),
+    })
+  ),
+  async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.readByEmail(email);
+
+      const { id: userId, password: hash } = user;
+
+      const match = await bcrypt.compare(password, hash);
+
+      if (match) {
+        const token = jwt.sign(
+          { userId },
+          process.env.JWT_SECRET,
+          { expiresIn: 3600 } // 1h
+        );
+
+        res.json({ auth: true, token });
+      } else {
+        throw new Error('User not found');
+      }
+    } catch (error) {
+      res.status(401).json({ error: 'User not found' });
+    }
+  }
+);
 
 // 404 handler
 router.use((req, res, next) => {
